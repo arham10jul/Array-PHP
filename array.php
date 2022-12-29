85e768f8c7a1634ddc06a735c0a12a91334ed1a3
Arham Mirza <102862412+arham10jul@users.noreply.github.com>
2022-12-29 18:40:54 +0500

    Add files via upload

---
+++ array.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+
+// $data=array(3);
+// $data[0]="Asif";
+// $data[1]="Ali";
+// $data[2]="Ahmed";
+// foreach($data as $a)
+// {
+//     echo $a." ";
+// }
+
+
+// $data=array("Asif","Ali","Amjad");
+// echo $data[0]." ";
+// echo $data[1]." ";
+// echo $data[2]." ";
+
+
+$data=array("Asif","Ali","Amjad");
+for($a=0; $a<3; $a++)
+{
+    echo $data[$a]." ";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
